5697fbeac2fe056344198d386d9602df4532d0b9
graffam <graffam87@gmail.com>
2015-02-02 15:08:30 -0500

    creates model for employee_animals

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,4 @@
 class Animal < ActiveRecord::Base
-  has_many :user_anmimals
-  has_many :employees, through: :user_animals
+  has_many :employee_anmimals
+  has_many :employees, through: :employee_animals
 end
